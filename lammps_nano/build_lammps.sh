24022fb3522972ca4a7784d5476bbd1458805426
EricPalermo <epalermo2@gmail.com>
2022-04-17 14:35:52 -0400

    added lammps build script

---
+++ lammps_nano/build_lammps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#copy external files into src folder
+cp LAMMPS-UEFEX/UEFEX/*.cpp lammps/src
+cp LAMMPS-UEFEX/UEFEX/*.h lammps/src
+#update CMakeLists.txt with links to external libs
+cp CMakeLists.txt lammps/cmake
+#Create a build folder
+mkdir build
+cd build
+#Compile and build lammps
+cmake ../cmake/ -D PKG_OPENMP=yes -D PKG_COLLOID=yes -D PKG_MOLECULE=yes -D PKG_UEF=yes -D PKG_MISC=yes -D PKG_RIGID=yes -D PKG_NETCDF=yes
+cmake --build .
