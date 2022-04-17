ab54c9791420cd4063b34a3acfbc36890c3ed24f
EricPalermo <epalermo2@gmail.com>
2022-04-17 15:37:33 -0400

    updated lammps build script

--- lammps_nano/build_lammps.sh
+++ lammps_nano/build_lammps.sh
@@ -4,13 +4,15 @@
 git submodule init
 git submodule update
 #copy external files into src folder
-cp LAMMPS-UEFEX/UEFEX/*.cpp lammps/src
-cp LAMMPS-UEFEX/UEFEX/*.h lammps/src
-#update CMakeLists.txt with links to external libs
+cp -r LAMMPS-UEFEX/UEFEX/ lammps/src/.
+mv lammps/src/UEFEX/domain.cpp lammps/src/.
+#update CMake files with references to UEFEX
 cp CMakeLists.txt lammps/cmake
+cp all_off.cmake lammps/cmake/presets
+cp all_on.cmake lammps/cmake/presets
 #Create a build folder
 mkdir lammps/build
 cd lammps/build
 #Compile and build lammps
-cmake ../cmake/ -D PKG_OPENMP=yes -D PKG_COLLOID=yes -D PKG_MOLECULE=yes -D PKG_UEF=yes -D PKG_MISC=yes -D PKG_RIGID=yes -D PKG_NETCDF=yes
+cmake ../cmake/ -D PKG_OPENMP=yes -D PKG_COLLOID=yes -D PKG_MOLECULE=yes -D PKG_UEF=yes -D PKG_UEFEX=yes -D PKG_MISC=yes -D PKG_RIGID=yes -D PKG_NETCDF=yes
 cmake --build .
