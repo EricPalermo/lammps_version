4302b5f7c4db02504c043a6131f3419b7560de4a
EricPalermo <epalermo2@gmail.com>
2022-05-04 13:32:30 -0400

    added helper files for building lammps

---
+++ lammps_nano_tersoff/build_lammps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Initiate and update submodules
+git submodule init
+git submodule update
+#copy external files into src folder
+cp -r LAMMPS-UEFEX/UEFEX/ lammps/src/.
+mv lammps/src/UEFEX/domain.cpp lammps/src/.
+cp tersoff_potential/pair_tersoff.* lammps/src/
+
+#update CMake files with references to UEFEX
+cp CMakeLists.txt lammps/cmake
+cp all_off.cmake lammps/cmake/presets
+cp all_on.cmake lammps/cmake/presets
+
+#Create a build folder
+mkdir lammps/build
+cd lammps/build
+
+#Compile and build lammps
+cmake ../cmake/ -D PKG_OPENMP=yes -D PKG_COLLOID=yes -D PKG_MOLECULE=yes -D PKG_UEF=yes -D PKG_UEFEX=yes -D PKG_MISC=yes -D PKG_RIGID=yes -D PKG_NETCDF=yes
+cmake --build . -j8
---
+++ lammps_nano_tersoff/readme.md
@@ -0,0 +1,10 @@
+LAMMPS Version: stable_29Sep2021 (https://github.com/lammps/lammps/tree/c30ba70fab466105925d02e8ec810f5fc7f9448b)
+External Packages:
+- LAMMPS-UEFEX (https://github.com/t-murash/LAMMPS-UEFEX/tree/948bd9d8f850d1fec6d3477cfc055c52e5caa02d)
+Custom Files (generated "in-house"):
+- tersoff_potential
+
+
+
+Requirements:
+- 
